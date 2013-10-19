f3a35bf18e8de2e1d426948a14aedf8ec782ae5a
gcmalloc <gcmalloc@gmail.com>
2013-10-19 13:01:36 +0200

    bootstrap fabric

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,9 @@
-from fabric.api import run, sudo, env
+from fabric.api import run, sudo, env, put
 from fabric.contrib.files import exists
 import cuisine
 
-env.ssh_config = True
+env.use_ssh_config = True
+env.user = "ubuntu"
 
 def uname():
     """Print the name of the system to stdout"""
@@ -13,6 +14,16 @@ def mongodb_install():
     """
     cuisine.package_install("mongodb")
     cuisine.upstart_ensure("mongodb-server")
+
+def install_fabric():
+    """Install fabric and cuisine on the host, use the package manager for 
+    fabric and pip for cuisine"""
+    cuisine.package_ensure("fabric")
+    cuisine.package_ensure("python-pip")
+    run("pip install --user cuisine")
+    put("fabfile.py")
+    put("ssh_conf", ".ssh/config")
+    put("~/.ssh/open_stack_test.pem", ".ssh/id_rsa")
 
 def update_upgrade():
     """Update the list of dependencies and upgrade the packages,
